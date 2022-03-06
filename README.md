2f6012b3e9441791e560eb2832ef89fddc34007c
Luis sanguino <luissanguino1702@gmail.com>
2022-03-06 13:18:54 -0400

    sockets

---
+++ README.md
@@ -0,0 +1,19 @@
+
+
+# Socket-Server
+
+Recostruir módulos de node
+````
+npm install
+````
+
+Generar el DIST
+````
+tsc -w
+````
+
+Levantar servidor, cualquiera de estos dos comandos
+````
+nodemon dist/
+node dist/
+````
---
+++ index.ts
@@ -0,0 +1,25 @@
+import Server from "./classes/server";
+import { SERVER_PORT } from "./global/enviroment";
+import router from './routes/router';
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+
+const server = new Server();
+
+// BodyParser
+// para generar un obj de javaScript, de lo q venga en el POST
+server.app.use( bodyParser.urlencoded( { extended: true } ) )
+server.app.use( bodyParser.json() )
+
+//CORS
+// para q cualquier persona pueda llamar los servicios desde otro hosting
+// en origin se puede agregar la url unica que dejara tener acceso, ( http://mipagina.com )
+server.app.use( cors({ origin: true, credentials: true}) )
+
+//Se crea la ruta de servicios
+server.app.use( '/', router )
+
+server.start( ()=>{
+    console.log( `Servidor corriendo en el puerto ${ SERVER_PORT }` )
+})
